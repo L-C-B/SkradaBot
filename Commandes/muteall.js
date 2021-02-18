eb6c9f0063a23fd260340c8d76c6f08476ddc11a
L-C-B <74789396+L-C-B@users.noreply.github.com>
2021-02-18 16:16:02 +0100

    Update muteall.js

--- Commandes/muteall.js
+++ Commandes/muteall.js
@@ -7,7 +7,7 @@ message.member.voice.channel.join()
     .setAuthor('Permission de mute nécessaire ', 'https://cdn.discordapp.com/emojis/793046366254268427.png?v=1')
     .setColor('#7bfbff')
     if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(EmBeD)
-    const channel = message.channel
+    const channel = message.member.voice.channel
     const members = channel.members
         members.forEach(member => {
             member.voice.setMute(true)
