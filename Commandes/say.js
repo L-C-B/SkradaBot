9e2f62cecf9bfe911ba6fa1711f8624068aaeed8
L-C-B <74789396+L-C-B@users.noreply.github.com>
2021-04-27 14:54:18 +0200

    Update say.js

--- Commandes/say.js
+++ Commandes/say.js
@@ -4,7 +4,7 @@ const config = require('../config.js');
 
 
 module.exports.run = (client, message, args) => {
-    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(':x: **| Vous n\'avez pas la permission d\'utiliser cette commande.**')
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(':x: **| Vous n\'avez pas la permission d\'utiliser cette commande.**')
     if (!args[0]) return message.reply(':pencil2: **| Vous devez indiquez du texte à envoyer !**')
     message.delete();
     var text = message.content.split(' ').slice(1).join(' ');
